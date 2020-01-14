a35e4b13e3f93f92f71d02f80a710edbc6866316
XUELUOCC <3196168302@qq.com>
2020-01-14 17:13:32 +0800

    colorPicker

--- src/common/colorPicker/colorPicker.vue
+++ src/common/colorPicker/colorPicker.vue
@@ -1,22 +1,110 @@
 <template>
   <div>
       <h3>颜色选择器</h3>
+      <div class="content">
+        <div class="colorPickerContent">
+          <div class="colorPicker">
+            <div class="left" ref="left"></div>
+            <div class="right" ref="right"></div>
+          </div>
+          <div class="roll"></div>
+        </div>
+        <div class="bottom">
+          <input type="text" class="inputs" v-model="color">
+          <div>
+            <span style="margin-right:10px;color:#409eff">清除</span>
+            <button class="btn">确定</button>
+          </div>     
+        </div>
+      </div>
+      
   </div>
+   
 </template>
 
 <script>
+
 export default {
   data () {
     return {
-      
+      color:'',
+  
     }
   },
+  mounted(){
+   
+  },
   methods: {
-
+    colorClick(e){
+      console.log(e)
+    },
+  
   }
 }
 </script>
 
 <style scoped>
 
+.content{
+  border:1px solid #ccc;
+  width:510px;
+  height:auto;
+  padding:10px;
+  margin-left:20px;
+}
+.colorPickerContent{
+  width:490px;
+  height:350px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.colorPicker{
+    width: 450px;
+    height: 350px;
+    position: relative;
+    background: rgb(213, 0, 255);
+}
+.left{
+  width:100%;
+  height:100%;
+  position:absolute;
+  background: linear-gradient(to right, #fff, rgba(255, 255, 255, 0));
+}
+.right{
+  width:100%;
+  height:100%;
+  position:absolute;
+  background: linear-gradient(to top, #000, transparent);
+}
+.roll{
+  width: 27px;
+  height: 350px;
+  background: linear-gradient(to bottom, red 0, #ff0 17%, #0f0 33%, #0ff 50%, #00f 67%, #f0f 83%, red 100%);
+}
+.bottom{
+  width:100%;
+  height:50px;
+   padding:5px;
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.inputs{
+  width:150px;
+  height:30px;
+  border:1px solid #ccc;
+  border-radius:4px;
+}
+.btn{
+  background-color:#fff;
+  color:#606266;
+  border:1px solid #ccc;
+  border-radius:4px;
+  padding: 4px 15px;
+}
+.btn:hover{
+  border:1px solid #409eff;
+  color:#409eff;
+}
 </style>
